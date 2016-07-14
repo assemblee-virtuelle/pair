1d5a73316af6562e2546d8817e13b38beee8dd96
jmvanel <jeanmarc.vanel@gmail.com>
2016-07-14 19:14:10 +0200

    FIX scripts après changements du nom de l'ontologie
    ajouté @context JSON-LD

--- check_ontology.sh
+++ check_ontology.sh
@@ -1,2 +1,2 @@
 echo See http://www.hermit-reasoner.com/command.html
-time java -jar ~/apps/hermit/HermiT.jar -cODk av-no-import.owl.ttl > resu_hermit.txt 2> resu2_hermit.txt &
+time java -jar ~/apps/hermit/HermiT.jar -cODk PAIR_1.0.owl.ttl > resu_hermit.txt 2> resu2_hermit.txt &
--- owl2jsonld.sh
+++ owl2jsonld.sh
@@ -2,6 +2,6 @@
 # via outil https://github.com/stain/owl2jsonld
 # ( je conseille de mettre tous ses projets git dans $HOME/src/ )
 
-OWLFILE=PAIR_5.owl.ttl
+OWLFILE=PAIR_1.0.owl.ttl
 java -jar $HOME/src/owl2jsonld/target/uberjar/owl2jsonld-0.2.2-SNAPSHOT-standalone.jar \
 	file://$HOME/src/pair/$OWLFILE > ${OWLFILE}.jsonld
