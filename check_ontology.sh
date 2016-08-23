93f988ec27ea9e4381bd20a29678a80854a84201
jmvanel <jeanmarc.vanel@gmail.com>
2016-08-23 16:38:25 +0200

    Fait la fusion manuelle pour le compte de Guillaume qui n'a pas fait un pull avant un commit
    Passé check_ontology.sh, mais ça ne repère pas ce genre de problèmes:
    TODO (jmv) : 2 problèmes sur foaf:currentProject:
    - on ne peut pas décemment lui ajouter un rdfs:range, il a en a déjà un! (foaf:Project)
    - de toute façon façon ce range est redondant à 80% avec celui de la super-Propriété :contributesTo
    
    Messages de Guillaume:
    Commit de ce matin sur l'ontologie : Ajouté les propriétés données spécifiques à l'ontologie PAIR, que l'on retrouvera pour la grande majorité dans les ontologies importées. Il était néanmoins important pour la v1 que l'on identifie les priprétés données fondamentales pour nous. Nous nous sommes dits avec Jean-Marc que nous fusionnerions les propriétés données ultérieurement, dans le cadre du travail sur la v2 par exemple... Suppression par ailleurs d'une erreur, sans doute liée au fait qu'on a supprimé avec Michel les "Domaines d'activités" sous la classe Idée.

--- check_ontology.sh
+++ check_ontology.sh
@@ -1,5 +1,5 @@
-echo Lunch HermiT reasonner, see http://www.hermit-reasoner.com/command.html
+echo Launch HermiT reasonner, see http://www.hermit-reasoner.com/command.html
 (
-  time java -jar ~/apps/hermit/HermiT.jar -cODk PAIR_1.0.owl.ttl > resu_hermit.txt 2> resu2_hermit.txt
+  time java -jar ~/apps/hermit/HermiT.jar -cODkU PAIR_1.0.owl.ttl > resu_hermit.txt 2> resu2_hermit.txt
   echo See results in resu_hermit.txt , or errors in resu2_hermit.txta
 ) &
